ede2c4d794757836e72b2960f1d9dfade0a411fd
root <root@DESKTOP-N4E07GN>
2024-12-20 12:54:11 +0500

    correction in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 // Serve the homepage with HTML content
 app.get('/', (req, res) => {
